0320e5090a7e1b095ab87645592b048b79b5ef54
dbarbozasoares <dbarboza.soares@gmail.com>
2024-10-21 14:12:01 -0400

    Last commit

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 // server.js
 const express = require('express');
 const path = require('path');
-
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
+// Use the CORS middleware
+app.use(cors());
+
+// Configure CORS for specific origins
+const corsOptions = {
+  origin: 'https://cameras-toronto.vercel.app/', // Replace with your frontend URL
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Allow credentials
+};
+app.use(cors(corsOptions));
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
